4a0ce12854ef50c0de01a438de0fb0227de33632
Jonathan Sax <jsax8310@outlook.com>
2017-10-23 14:00:01 -0600

    added queries

--- models/companies.js
+++ models/companies.js
@@ -16,7 +16,21 @@ module.exports = {
     Company.find(callback).limit(3)
   },
 
+  getCompanyById: function(id, callback) {
+    Company.findById(id,callback);
+  },
+
   addCompany: function(company, callback) {
     company.create(company, callback)
+  },
+
+  updateBabyInfo: function(id, company, options, callback) {
+    let query = {_id:id};
+    Company.findOneAndUpdate(query, company, options, callback);
+  },
+
+  deleteCompanyInfo: function(id, callback) {
+    let query = {_id:id}
+    Company.remove(query, callback)
   }
 }
