98c1338e148583eafc3cf9355579f47c4f21b78d
Jonathan Sax <jsax8310@outlook.com>
2017-11-01 10:31:34 -0600

    changed hashtag to a dev feed

--- routes/twitter.js
+++ routes/twitter.js
@@ -34,7 +34,7 @@ router.get('/hashtag', function(req, res) {
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
   });
 
-  var params = {q: '#RTPodcast'}
+  var params = {q: '#vuejs'}
   client.get('search/tweets', params, function(error, tweets, response) {
     if (!error) {
       res.json(tweets)
